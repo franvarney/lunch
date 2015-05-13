fefcd9df182b63718392404e0228795926a72f0e
Fran Varney <fran@modulus.io>
2015-05-13 13:46:41 -0400

    Convert to module
    Convert to module, refactor, and add readme

--- scripts/picker.js
+++ scripts/picker.js
@@ -1,31 +1,29 @@
 var _          = require('lodash');
 var fs         = require('fs');
-var placesPath = './data/places.json';
-var lastPath   = './data/last.txt';
+
+var path     = './data/places.json';
+var data     = require('.' + path);
+var lastPath = './data/last.txt';
 
 function random (array) {
   return _.shuffle(array)[0];
 }
 
 module.exports = {
-  // Pick a random place (and ignore the last random place)
+  // Pick a random place (and ignore the last randomly suggested place)
   pick: function () {
-    fs.readFile(placesPath, 'utf8', function (err, data) {
-      if (err) throw err;
-      var object = JSON.parse(data);
-      var placesArray = [];
-      var lastPlace = fs.readFileSync(lastPath, 'utf8');
-      object.places.forEach(function (places, index) {
-        placesArray.push(places);
-        if (lastPlace === places.place) {
-          placesArray.splice(index, 1);
-        }
-        if (index + 1 === object.places.length) {
-          var result = random(placesArray).place;
-          fs.writeFileSync(lastPath, result);
-          return console.log(result);
-        }
-      });
+    var placesArray = [];
+    var lastPlace = fs.readFileSync(lastPath, 'utf8');
+    data.places.forEach( function (places, index) {
+      placesArray.push(places);
+      if (lastPlace === places.place) {
+        placesArray.splice(index, 1);
+      }
+      if (index + 1 === data.places.length) {
+        var result = random(placesArray).place;
+        fs.writeFileSync(lastPath, result);
+        return console.log(result);
+      }
     });
   }
 };
--- scripts/place.js
+++ scripts/place.js
@@ -1,63 +1,47 @@
-var fs       = require('fs');
-var dataPath = './data/places.json';
+var fs   = require('fs');
+var path = './data/places.json';
+var data = require('.' + path);
 
 module.exports = {
   // List all places to eat
   list: function () {
-    fs.readFile(dataPath, 'utf8', function (err, data) {
-      if (err) throw err;
-      var object = JSON.parse(data);
-      var placesArray = [];
-      object.places.forEach(function (places, index) {
-        placesArray.push(places.place);
-        if (index + 1 === object.places.length) {
-          var sorted = (placesArray.sort()).toString();
-          return console.log(sorted.replace(/,/g, '\n'));
-        }
-      });
+    data.places.sort( function (a, b) {
+      return a.place > b.place;
+    }).forEach( function (places, index) {
+      return console.log(places.place);
     });
   },
   // Add a new place to eat
   new: function (newPlace) {
-    fs.readFile(dataPath, 'utf8', function (err, data) {
-      if (err) throw err;
-      var object = JSON.parse(data);
-      object.places.forEach(function (places, index) {
-        if (newPlace === places.place) {
-          return console.log(newPlace + ' has already been added!');
-        }
-        if (newPlace !== places.place && index + 1 === object.places.length) {
-          object.places.push({ "place" : newPlace });
-          fs.writeFile(dataPath, JSON.stringify(object), function(err) {
-            if (err) throw err;
-            return console.log(newPlace + ' was added!');
-          });
-        };
-      });
+    data.places.forEach( function (places, index) {
+      if (newPlace === places.place) {
+        return console.log(newPlace + ' has already been added!');
+      }
+      if (newPlace !== places.place && index + 1 === data.places.length) {
+        data.places.push({ 'place' : newPlace });
+        fs.writeFile(path, JSON.stringify(data, null, 2), function (err) {
+          if (err) throw err;
+          return console.log(newPlace + ' was added!');
+        });
+      };
     });
   },
   // Remove a place to eat
   remove: function (place) {
-    fs.readFile(dataPath, 'utf8', function (err, data) {
-      if (err) throw err;
-      var object = JSON.parse(data);
-      var placesArray = [];
-      var objectLength = object.places.length;
-      object.places.forEach(function (places, index) {
-        if (place !== places.place && index + 1 === object.places.length) {
-          return console.log(place + ' doesn\'t exist!');
-        }
-        placesArray.push(places.place);
-        if (place === places.place) {
-          object.places.splice(index, 1);
-        }
-        if (index + 1 === objectLength) {
-          fs.writeFile(dataPath, JSON.stringify(object), function(err) {
-            if (err) throw err;
-            return console.log(place + ' was removed!');
-          });
-        }
-      });
+    var dataLength = data.places.length;
+    data.places.forEach( function (places, index) {
+      if (place !== places.place && index + 1 === dataLength) {
+        return console.log(place + ' doesn\'t exist!');
+      }
+      if (place === places.place) {
+        data.places.splice(index, 1);
+      }
+      if (index + 1 === dataLength) {
+        fs.writeFile(path, JSON.stringify(data, null, 2), function (err) {
+          if (err) throw err;
+          return console.log(place + ' was removed!');
+        });
+      }
     });
   }
 };
